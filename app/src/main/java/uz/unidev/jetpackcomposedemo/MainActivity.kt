2aaffb2f0ebb7f0dbf4060e55c1980336ddad79a
Nurlibay Koshkinbaev <koshkinbayevn@gmail.com>
2022-09-20 01:49:28 +0500

    Modifiers

--- app/src/main/java/uz/unidev/jetpackcomposedemo/MainActivity.kt
+++ app/src/main/java/uz/unidev/jetpackcomposedemo/MainActivity.kt
@@ -1,12 +1,14 @@
 package uz.unidev.jetpackcomposedemo
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -15,17 +17,33 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
+            Column(
                 modifier = Modifier
-                    .width(200.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+//                    .padding(top = 50.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+//                    .width(600.dp)
+//                    .requiredWidth(600.dp)
             ) {
-                Text("Hello")
-                Text("World")
-                Text("Android")
+                Text(text = "Hello", modifier = Modifier
+                    .clickable {
+                        Toast.makeText(this@MainActivity, "This is text", Toast.LENGTH_SHORT).show()
+                    }
+//                    .border(5.dp, Color.Yellow)
+//                    .padding(5.dp)
+//                    .offset(20.dp, 20.dp)
+//                    .border(10.dp, Color.Black)
+//                    .padding(10.dp)
+                )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(text = "World")
             }
         }
     }
